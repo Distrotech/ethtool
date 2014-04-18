72b683b4e43dc0f1b90192651c2378bba3db4207
Amos Kong <akong@redhat.com>
2014-04-18 09:39:59 +0800

    ethtool: fix a typo in displaying channel parameters
     # ethtool -L eth0 combined 1
     combined unmodified, ignoring
     no channel parameters changed, aborting
     current values: tx 0 rx 0 other 0combined 1
                                    ^^^^
    
    Signed-off-by: Amos Kong <akong@redhat.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- ethtool.c
+++ ethtool.c
@@ -1770,7 +1770,7 @@ static int do_schannels(struct cmd_context *ctx)
 	if (!changed) {
 		fprintf(stderr, "no channel parameters changed, aborting\n");
 		fprintf(stderr, "current values: tx %u rx %u other %u"
-			"combined %u\n", echannels.rx_count,
+			" combined %u\n", echannels.rx_count,
 			echannels.tx_count, echannels.other_count,
 			echannels.combined_count);
 		return 1;
