Distrotech/ethtool

commit fef1c4a19703ddf939dd7ef18e3994134dc2aea0

ethtool: add get permanent address option
Add command level support for showing permanent address.
The ioctl has been around for a long time but there was
no option to display it.

Note: MAX_ADDR_LEN is defined in netdevice.h but including
netdevice.h leads to multiple definition errors with if.h.

Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
[bwh: Fix use of '|' in place of '||', as noted by Joe Perches]
Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>

ethtool.c
@@ -53,6 +53,9 @@
 #ifndef SIOCETHTOOL
 #define SIOCETHTOOL     0x8946
 #endif
+#ifndef MAX_ADDR_LEN
+#define MAX_ADDR_LEN	32
+#endif
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #endif
@@ -110,6 +113,8 @@ static int do_srxfhindir(int fd, struct ifreq *ifr);
 static int do_srxntuple(int fd, struct ifreq *ifr);
 static int do_grxntuple(int fd, struct ifreq *ifr);
 static int do_flash(int fd, struct ifreq *ifr);
+static int do_permaddr(int fd, struct ifreq *ifr);
+
 static int send_ioctl(int fd, struct ifreq *ifr);
 
 static enum {
@@ -139,6 +144,7 @@ static enum {
 	MODE_SNTUPLE,
 	MODE_GNTUPLE,
 	MODE_FLASHDEV,
+	MODE_PERMADDR,
 } mode = MODE_GSET;
 
 static struct option {
@@ -256,6 +262,8 @@ static struct option {
 		"		action N\n" },
     { "-u", "--show-ntuple", MODE_GNTUPLE,
 		"Get Rx ntuple filters and actions\n" },
+    { "-P", "--show-permaddr", MODE_PERMADDR,
+		"Show permanent hardware address" },
     { "-h", "--help", MODE_HELP, "Show this help" },
     {}
 };
@@ -826,7 +834,8 @@ static void parse_cmdline(int argc, char **argp)
 			    (mode == MODE_SNTUPLE) ||
 			    (mode == MODE_GNTUPLE) ||
 			    (mode == MODE_PHYS_ID) ||
-			    (mode == MODE_FLASHDEV)) {
+			    (mode == MODE_FLASHDEV) ||
+			    (mode == MODE_PERMADDR)) {
 				devname = argp[i];
 				break;
 			}
@@ -1997,6 +2006,8 @@ static int doit(void)
 		return do_grxntuple(fd, &ifr);
 	} else if (mode == MODE_FLASHDEV) {
 		return do_flash(fd, &ifr);
+	} else if (mode == MODE_PERMADDR) {
+		return do_permaddr(fd, &ifr);
 	}
 
 	return 69;
@@ -3075,6 +3086,31 @@ static int do_flash(int fd, struct ifreq *ifr)
 	err = send_ioctl(fd, ifr);
 	if (err < 0)
 		perror("Flashing failed");
+
+	return err;
+}
+
+static int do_permaddr(int fd, struct ifreq *ifr)
+{
+	int i, err;
+	struct ethtool_perm_addr *epaddr;
+
+	epaddr = malloc(sizeof(struct ethtool_perm_addr) + MAX_ADDR_LEN);
+	epaddr->cmd = ETHTOOL_GPERMADDR;
+	epaddr->size = MAX_ADDR_LEN;
+	ifr->ifr_data = (caddr_t)epaddr;
+
+	err = send_ioctl(fd, ifr);
+	if (err < 0)
+		perror("Cannot read permanent address");
+	else {
+		printf("Permanent address:");
+		for (i = 0; i < epaddr->size; i++)
+			printf("%c%02x", (i == 0) ? ' ' : ':',
+			       epaddr->data[i]);
+		printf("\n");
+	}
+	free(epaddr);
 
 	return err;
 }

ethtool.8
@@ -176,6 +176,9 @@ ethtool \- Display or change ethernet card settings
 .I ethX
 .RI [ N ]
 
+.B ethtool \-P|\-\-show-permaddr
+.I ethX
+
 .B ethtool \-r|\-\-negotiate
 .I ethX
 
@@ -402,6 +405,9 @@ blinking one or more LEDs on the specific ethernet port.
 .TP
 .B N
 Length of time to perform phys-id, in seconds.
+.TP
+.B \-P \-\-show-permaddr
+Queries the specified ethernet device for permanent hardware address.
 .TP
 .B \-r \-\-negotiate
 Restarts auto-negotiation on the specified ethernet device, if

commit d70e1ebc02dea8082d605c96525e966305c792e4

ethtool: avoid NULL pointer dereference in do_permaddr
Coverity issue: 1363118
Fixes: fef1c4a19703 ("ethtool: add get permanent address option")

Signed-off-by: John W. Linville <linville@tuxdriver.com>
Reviewed-by: Greg Rose <grose@lightfleet.com>

ethtool.c
@@ -3941,6 +3941,11 @@ static int do_permaddr(struct cmd_context *ctx)
 	struct ethtool_perm_addr *epaddr;
 
 	epaddr = malloc(sizeof(struct ethtool_perm_addr) + MAX_ADDR_LEN);
+	if (!epaddr) {
+		perror("Cannot allocate memory for operation");
+		return 1;
+	}
+
 	epaddr->cmd = ETHTOOL_GPERMADDR;
 	epaddr->size = MAX_ADDR_LEN;
 